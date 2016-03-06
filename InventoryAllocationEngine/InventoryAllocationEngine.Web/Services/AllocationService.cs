1767b9bc8fe3aa1abc8e31499b3c5cb4c68cc325
Lee Dumond <leedumond@gmail.com>
2016-03-06 17:08:31 -0600

    refactoring of allocation services

--- InventoryAllocationEngine/InventoryAllocationEngine.Web/Services/AllocationService.cs
+++ InventoryAllocationEngine/InventoryAllocationEngine.Web/Services/AllocationService.cs
@@ -27,51 +27,70 @@ namespace InventoryAllocationEngine.Web.Services
             return;
          }
 
-         int quantityAllocated = 0;
-         List<OrderItem> orderItemsSorted = product.OrderItems.ToList();
-
          if (allocationMethod == AllocationMethod.Simple)
          {
-
-            orderItemsSorted = product.OrderItems.OrderByDescending(oi => oi.QuantityOrdered).ToList();
-            double percentage = (double)onHand / quantityOrdered;
-
-            foreach (var orderItem in orderItemsSorted)
-            {
-               orderItem.QuantityAllocated = (int)(orderItem.QuantityOrdered * percentage);
-            }
-
-            quantityAllocated = product.OrderItems.Sum(oi => oi.QuantityAllocated);
-
+            AllocateSimple(product);
          }
 
          if (allocationMethod == AllocationMethod.LargestOrderFirst)
          {
-            orderItemsSorted = product.OrderItems.OrderByDescending(oi => oi.QuantityOrdered).ToList();
-
-            int amountRemaining = onHand;
-            int index = 0;
-
-            while (amountRemaining > 0 && index+1 <= orderItemsSorted.Count)
-            {
-               var currentOrderItem = orderItemsSorted[index];
-
-               if (amountRemaining > currentOrderItem.QuantityOrdered)
-               {
-                  currentOrderItem.QuantityAllocated = currentOrderItem.QuantityOrdered;
-                  quantityAllocated += currentOrderItem.QuantityAllocated;
-               }
-               else
-               {
-                  currentOrderItem.QuantityAllocated = amountRemaining;
-                  quantityAllocated += amountRemaining;
-               }
-
-               amountRemaining -= currentOrderItem.QuantityAllocated;
-               
-               index++;
-            }
+            AllocateLargestOrderFirst(product);
          }
+
+      }
+
+      private void AllocateLargestOrderFirst(Product product)
+      {
+         int quantityAllocated = 0;
+         var orderItemsSorted = product.OrderItems.OrderByDescending(oi => oi.QuantityOrdered).ToList();
+         int onHand = product.QuantityAvailable;
+
+         int amountRemaining = onHand;
+         int index = 0;
+
+         while (amountRemaining > 0 && index + 1 <= orderItemsSorted.Count)
+         {
+            var currentOrderItem = orderItemsSorted[index];
+
+            if (amountRemaining > currentOrderItem.QuantityOrdered)
+            {
+               currentOrderItem.QuantityAllocated = currentOrderItem.QuantityOrdered;
+               quantityAllocated += currentOrderItem.QuantityAllocated;
+            }
+            else
+            {
+               currentOrderItem.QuantityAllocated = amountRemaining;
+               quantityAllocated += amountRemaining;
+            }
+
+            amountRemaining -= currentOrderItem.QuantityAllocated;
+
+            index++;
+         }
+
+         int unallocated = onHand - quantityAllocated;
+
+         if (unallocated > 0)
+         {
+            orderItemsSorted.First().QuantityAllocated = orderItemsSorted.First().QuantityAllocated + unallocated;
+         }
+
+      }
+
+      private void AllocateSimple(Product product)
+      {
+         var orderItemsSorted = product.OrderItems.OrderByDescending(oi => oi.QuantityOrdered).ToList();
+         int onHand = product.QuantityAvailable;
+         int quantityOrdered = product.OrderItems.Sum(oi => oi.QuantityOrdered);
+
+         double percentage = (double)onHand / quantityOrdered;
+
+         foreach (var orderItem in orderItemsSorted)
+         {
+            orderItem.QuantityAllocated = (int)(orderItem.QuantityOrdered * percentage);
+         }
+
+         var quantityAllocated = product.OrderItems.Sum(oi => oi.QuantityAllocated);
 
          int unallocated = onHand - quantityAllocated;
 
